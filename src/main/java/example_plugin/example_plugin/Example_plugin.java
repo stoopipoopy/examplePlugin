de545f0a7f5dfb41dd480e51b0da47dcd3f0f21e
petropapahadjopoulos <anotherguywithabeanie@gmail.com>
2022-07-12 18:06:04 -0700

    Initial commit

---
+++ src/main/java/example_plugin/example_plugin/Example_plugin.java
@@ -0,0 +1,17 @@
+package example_plugin.example_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Example_plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
